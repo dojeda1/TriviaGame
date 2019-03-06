8aac30d951a9e51a256df53b3c4e72f95b7b1998
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-03-06 15:34:43 -0700

    can choose right or wrong answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,47 +3,47 @@
 var easyQuestions = {
 
     question1: {
+        prompt: "How many meters are in a kilometer?",
+        correctAnswer: "1000",
         choices: [
             "1000",
             "100",
             "10",
             "10,000"
         ],
-        correctAnswer: "1000",
-        prompt: "How many meters are in a kilometer?"
     },
 
     question2: {
+        prompt: "How many centimeters are in a meter?",
+        correctAnswer: "100",
         choices: [
             "1000",
             "100",
             "10",
             "10,000"
         ],
-        correctAnswer: "100",
-        prompt: "How many centimeters are in a meter?"
     },
 
     question3: {
+        prompt: "How many millimeters are in a meter?",
+        correctAnswer: "1000",
         choices: [
             "1000",
             "100",
             "10",
             "10,000"
         ],
-        correctAnswer: "1000",
-        prompt: "How many millimeters are in a meter?"
     },
 
     question4: {
+        prompt: "How many millimeters are in a kilometer?",
+        correctAnswer: "1,000,000",
         choices: [
             "100,000,000",
             "10,000",
             "1000",
             "1,000,000"
         ],
-        correctAnswer: "1,000,000",
-        prompt: "How many millimeters are in a kilometer?"
     },
 
 };
@@ -51,54 +51,113 @@ var easyQuestions = {
 var hardQuestions = {
 
     question1: {
+        prompt: "How many feet are in a mile?",
+        correctAnswer: "5280",
         choices: [
             "5280",
             "3560",
             "2000",
             "4960"
         ],
-        correctAnswer: "5280",
-        prompt: "How many feet are in a mile?"
     },
 
     question2: {
+        prompt: "How many inches are in a foot?",
+        correctAnswer: "12",
         choices: [
             "12",
             "18",
             "10",
             "8"
         ],
-        correctAnswer: "12",
-        prompt: "How many inches are in a foot?"
     },
 
     question3: {
+        prompt: "How many feet are in a yard?",
+        correctAnswer: "3",
         choices: [
             "3",
             "4",
             "5",
             "8"
         ],
-        correctAnswer: "3",
-        prompt: "How many feet are in a yard?"
     },
 
     question4: {
+        prompt: "How many inches are in a mile?",
+        correctAnswer: "63,360",
         choices: [
             "63,360",
             "82,600",
             "44,720",
             "52,000"
         ],
-        correctAnswer: "63,360",
-        prompt: "How many inches are in a mile?"
     },
 
 };
 
+var correct = 0;
+var incorrect = 0;
+
 var playMode = {};
 
-$(".startButton").on("click", function () {
-    playMode = easyQuestions;
+function shuffle(sourceArray) {
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+        var temp = sourceArray[j];
+        sourceArray[j] = sourceArray[i];
+        sourceArray[i] = temp;
+    }
+    return sourceArray;
+};
+
+function playGame() {
+
+    correct = 0;
+    incorrect = 0;
+
+    console.log("Playing Game");
+
+    shuffle(playMode);
     console.log(playMode);
+
+
+
+    $("#currentQuestion").html(playMode.question1.prompt);
+    $("#ans1").html(playMode.question1.choices[0]);
+    $("#ans2").html(playMode.question1.choices[1]);
+    $("#ans3").html(playMode.question1.choices[2]);
+    $("#ans4").html(playMode.question1.choices[3]);
+    console.log(playMode.question1.correctAnswer);
+
+    $(".ansButton").on("click", function () {
+
+        var yourAnswer = this.innerHTML;
+        console.log(yourAnswer);
+
+        if (yourAnswer === playMode.question1.correctAnswer) {
+            correct++;
+        } else {
+            incorrect++
+        }
+
+        console.log("right: " + correct + ",wrong: " + incorrect)
+    });
+
+
+};
+
+$(".startButton").on("click", function () {
+    var selection = this.id
+    if (selection === "easyButton") {
+        playMode = easyQuestions;
+    } else if (selection === "hardButton") {
+        playMode = hardQuestions;
+    }
+    console.log(playMode);
+    playGame();
 });
+
+
+// end of game function
